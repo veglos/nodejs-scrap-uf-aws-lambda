350f391151124a6b08097e46a30301ddde47a5a0
Carlos Vega <cvegah01@gmail.com>
2022-10-31 19:02:30 -0300

    adds cache

--- src/uf.ts
+++ src/uf.ts
@@ -3,8 +3,10 @@ import * as cheerio from 'cheerio';
 
 const url = 'https://si3.bcentral.cl/indicadoressiete/secure/IndicadoresDiarios.aspx';
 const AxiosInstance = axios.create();
+const cacheTimeInMinutes = 5;
 
 let cachedUF: number = -1;
+let refreshCacheTime: Date = new Date();
 
 export async function getUF(): Promise<number> {
 
@@ -12,16 +14,17 @@ export async function getUF(): Promise<number> {
         const response = await AxiosInstance.get(url);
         const html = response.data;
         const uf = scrapUFValue(html);
+        console.log('### fresh ###');
         cachedUF = uf;
-        return cachedUF;
     }
-    else {
-        return cachedUF;
-    }
+
+    console.log('### cached ###');
+    return cachedUF;
 }
 
 function refreshCache(): boolean {
-    return true;
+    if (cachedUF == -1) return true;
+    return (new Date().getMinutes() - refreshCacheTime.getMinutes() > cacheTimeInMinutes);
 }
 
 function scrapUFValue(html: string): number {
